4585c4047729ab48e1aed5dd0bc580abb754036d
Nour Souid <noursouidd@gmail.com>
2020-05-30 21:11:02 -0400

    api call

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,6 +1,6 @@
 class Currency < ApplicationRecord
   def current_price
-    url = 'https://api.coinmarketcap.com/v1/ticker/'
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD'
     request = HTTParty.get(url + self.slug)
     response = JSON.parse(request.body)
   end
